dbc823e0ac4f6f68e149e59eafcc4d1b6120efb4
ImAlpha <arcticjcb@gmail.com>
2024-04-17 21:32:14 -0700

    first two pages linked

--- Flask_Blog_Src/flaskBlog.py
+++ Flask_Blog_Src/flaskBlog.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__) # name of the module, flask knows where too look for files
+
+posts =[
+    {
+        'author': 'Jacob Delgado',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 16, 2024'
+    },
+    {
+        'author': 'Keiwa on the Kob',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 18, 2024'
+    },
+]
+
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title="About")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    
