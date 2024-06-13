b1c8c2978afc7cac08862cfb91bbe2a474cedf8f
ImAlpha <arcticjcb@gmail.com>
2024-06-13 00:41:30 -0700

    bootstrap added and commented out for nextjs frontend

--- Flask_Blog_Src/flaskBlog.py
+++ Flask_Blog_Src/flaskBlog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__) # name of the module, flask knows where too look for files
+CORS(app)
 
 posts =[
     {
@@ -21,13 +24,18 @@ posts =[
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    # return render_template('home.html', posts=posts)
+    return jsonify({ 
+        'author': 'Jacob Delgado',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 16, 2024'})
 
 @app.route("/about")
 def about():
     return render_template('about.html', title="About")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=7080)
     
     
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Flask-REST
 Learning Flask by creating a REST API
+
+`export FLASK_APP=flaskBlog.py`
+or for Windows
+`set FLASK_APP=flaskBlog.py`
+
+`flask run`
