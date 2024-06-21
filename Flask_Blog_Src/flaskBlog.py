d78b3431abd96b3fcbeacbc70d28d0e66b896c67
ImAlpha <arcticjcb@gmail.com>
2024-06-20 18:30:00 -0700

    part 3 tutorial done

--- Flask_Blog_Src/flaskBlog.py
+++ Flask_Blog_Src/flaskBlog.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__) # name of the module, flask knows where too look for files
 CORS(app)
+
+app.config['SECRET_KEY'] = 'dgwrF*5USdWAGr4EqFPqYK'
 
 posts =[
     {
@@ -24,16 +27,31 @@ posts =[
 @app.route("/")
 @app.route("/home")
 def home():
-    # return render_template('home.html', posts=posts)
-    return jsonify({ 
-        'author': 'Jacob Delgado',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 16, 2024'})
+    return render_template('home.html', posts=posts)
+    # return jsonify({ 
+    #     'author': 'Jacob Delgado',
+    #     'title': 'Blog Post 1',
+    #     'content': 'First post content',
+    #     'date_posted': 'April 16, 2024'})
 
 @app.route("/about")
 def about():
     return render_template('about.html', title="About")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=7080)
